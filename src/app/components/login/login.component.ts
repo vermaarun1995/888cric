8e1f80363203586ce188f07bed7881f211946a84
vermaarun1995 <vermaarun1995@gmail.com>
2022-10-17 20:23:56 +0530

    login and register implement

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConfirmService } from 'src/app/services/confirm.service';
+import { HttpService } from 'src/app/services/http.service';
+import { ResponseModel } from 'src/app/models/responseModel';
+import { AuthService } from 'src/app/services/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  isSubmitted = false;
+  formSubmitError: string = "";
+
+  constructor(private formBuilder: FormBuilder, private confirmService: ConfirmService, private service: HttpService, private authService: AuthService, private modalService: NgbModal) {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+   }
+
+   loginFormSubmit() {    
+    this.isSubmitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }    
+    //this.confirmService.openConfirmBox(true);
+
+    this.confirmService.getConfirmData().subscribe(
+      (res) => {
+        if (res == false) {
+          this.service.post('Account/Login', this.loginForm.value)
+            .subscribe((response: ResponseModel) => {
+              if (response.status == 200 && response.data != null) {
+                this.formSubmitError = "";
+                this.authService.login(response.data);
+                this.modalService.dismissAll();
+              }
+              if (response.data == null) {
+                this.formSubmitError = response.message;
+              }
+            });
+        }
+      }
+    );
+  }
 
   ngOnInit(): void {
-  }
+  } 
 
 }
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConfirmService } from 'src/app/services/confirm.service';
+import { HttpService } from 'src/app/services/http.service';
+import { ResponseModel } from 'src/app/models/responseModel';
+import { AuthService } from 'src/app/services/auth.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +13,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  isSubmitted = false;
+  formSubmitError: string = "";
+
+  constructor(private formBuilder: FormBuilder, private confirmService: ConfirmService, private service: HttpService, private authService: AuthService, private modalService: NgbModal) {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      fullname: ['', Validators.required],
+      email: ['', Validators.required],
+      phonenumber: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmpassword: ['', Validators.required]
+    });
+   }
+
+   registerFormSubmit(){
+    debugger;
+    this.isSubmitted = true;
+    if (this.registerForm.invalid) {
+      alert("invalid parameters");
+      return;
+    }    
+
+    if(this.registerForm.value.password != this.registerForm.value.confirmpassword){
+      alert("Password and confirm password not match");
+      return;
+    }
+
+    this.confirmService.getConfirmData().subscribe(
+      (res) => {
+        if (res == false) {
+          this.service.post('Account/Register', this.registerForm.value)
+            .subscribe((response: ResponseModel) => {
+              if (response.status == 200 && response.data != null) {
+                this.formSubmitError = "";
+                this.authService.login(response.data);
+                this.modalService.dismissAll();
+              }
+              if (response.data == null) {
+                this.formSubmitError = response.message;
+                alert(response.message);
+              }
+            });
+        }
+      }
+    );
+
+   }
 
   ngOnInit(): void {
   }
