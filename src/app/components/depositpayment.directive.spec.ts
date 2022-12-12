cea66f042dcfa525a5df748bdb45c8c57ed2174d
vermaarun1995 <vermaarun1995@gmail.com>
2022-12-12 21:49:36 +0530

    match odds issue and opening balance issue resolved

---
+++ src/app/components/depositpayment.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DepositpaymentDirective } from './depositpayment.directive';
+
+describe('DepositpaymentDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DepositpaymentDirective();
+    expect(directive).toBeTruthy();
+  });
+});
